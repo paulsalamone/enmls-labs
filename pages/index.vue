5d5f2aa71010b786f0b0e646acf99b05756c2683
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2023-06-16 09:45:06 +0200

    add homepage notes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h1>EMNLS</h1>
+    <p>
+      <i
+        >ABOUT: This is a more "permanent" place to show / practice variious
+        libraries and experiences within vue framework, all while practising
+        deeper nuxt stuff to keep it all going. Goal is an app that i and others
+        can use to make interesting creative stuff and save it to a server (i.e.
+        friebase etc)</i
+      >
+    </p>
+    <h3>next steps:</h3>
+    <ul>
+      <li>create dynamic SLUG page</li>
+      <li>make a few projects/components with it</li>
+      <li>use to generate nav list</li>
+    </ul>
+    <h3>also:</h3>
+    <ul>
+      <li>make first random grid generator + localstorage saver</li>
+      <li>
+        add some kind of global state (i.e. highlight color hex code setter)
+      </li>
+    </ul>
   </div>
 </template>
   
